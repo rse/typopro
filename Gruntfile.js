c3481b464ca398e66bc71227249d43028885784a
Ralf S. Engelschall <rse@engelschall.com>
2014-01-16 15:32:27 +0100

    provide initial cut-through for TypoPRO.org website

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+
+/* global module: true */
+module.exports = function (grunt) {
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-bower-install-simple");
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        "bower-install-simple": {
+            options: {
+                color:       true,
+                production:  true,
+                directory:   "bower_components"
+            }
+        },
+        copy: {
+            "jquery": {
+                src: [ "bower_components/jquery/jquery.js" ],
+                dest: "lib/jquery/jquery.js"
+            },
+            "font-awesome-css": {
+                src: [ "bower_components/font-awesome/css/font-awesome.css" ],
+                dest: "lib/font-awesome/font-awesome.css",
+                options: {
+                    process: function (content, srcpath) {
+                        return content.replace(/\.\.\/fonts\//g, "");
+                    }
+                }
+            },
+            "font-awesome-fonts": {
+                files: [{
+                    expand: true, flatten: false, cwd: "bower_components/font-awesome/fonts",
+                    src: "fontawesome-webfont.*",
+                    dest: "lib/font-awesome/"
+                }]
+            },
+            "typopro": {
+                files: [
+                    { expand: true, flatten: false, cwd: "bower_components/typopro/web",
+                      src: "TypoPRO-OpenSans/**", dest: "lib/typopro/" },
+                    { expand: true, flatten: false, cwd: "bower_components/typopro/web",
+                      src: "TypoPRO-DejaVu/**", dest: "lib/typopro/" },
+                    { expand: true, flatten: false, cwd: "bower_components/typopro/web",
+                      src: "**", dest: "specimen/" },
+                ]
+            },
+            "typopro-index": {
+                src: [ "bower_components/typopro/index.html" ],
+                dest: "specimen/index.html",
+                options: {
+                    process: function (content, srcpath) {
+                        return content.replace(/web\//g, "");
+                    }
+                }
+            },
+        },
+        clean: {
+            clean:     [ "lib" ],
+            distclean: [ "node_modules", "bower_components" ]
+        }
+    });
+
+    grunt.registerTask("default", [ "bower-install-simple", "copy" ]);
+};
+
---
+++ README.md
@@ -0,0 +1,6 @@
+
+Gemstone
+========
+
+This is the Gemstone website (Web: http://gemstonejs.com/).
+
