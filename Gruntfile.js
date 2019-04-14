4d841ec13c944cbb479a97e379e2d6e2c47864bd
Ralf S. Engelschall <rse@engelschall.com>
2019-04-14 17:15:10 +0200

    upgrade to TypoPRO 4.2.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,48 +1,38 @@
 
 /* global module: true */
 module.exports = function (grunt) {
-    grunt.loadNpmTasks("grunt-contrib-clean");
-    grunt.loadNpmTasks("grunt-contrib-copy");
-    grunt.loadNpmTasks("grunt-bower-install-simple");
-
+    grunt.loadNpmTasks("grunt-contrib-clean")
+    grunt.loadNpmTasks("grunt-contrib-copy")
+    grunt.loadNpmTasks("grunt-bower-install-simple")
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
-        "bower-install-simple": {
-            main: {
-                options: {
-                    color:       true,
-                    production:  true,
-                    directory:   "bower_components"
-                }
-            }
-        },
         copy: {
             "normalize": {
-                src: [ "bower_components/normalize.css/normalize.css" ],
+                src: [ "node_modules/normalize.css/normalize.css" ],
                 dest: "lib/normalize/normalize.css"
             },
             "jquery": {
-                src: [ "bower_components/jquery/dist/jquery.js" ],
+                src: [ "node_modules/jquery/dist/jquery.js" ],
                 dest: "lib/jquery/jquery.js"
             },
             "typopro": {
                 files: [
-                    { expand: true, flatten: false, cwd: "bower_components/typopro-web/web",
+                    { expand: true, flatten: false, cwd: "node_modules/typopro-web/web",
                       src: "TypoPRO-OpenSans/**", dest: "lib/typopro/" },
-                    { expand: true, flatten: false, cwd: "bower_components/typopro-web/web",
+                    { expand: true, flatten: false, cwd: "node_modules/typopro-web/web",
                       src: "TypoPRO-DejaVu/**", dest: "lib/typopro/" },
-                    { expand: true, flatten: false, cwd: "bower_components/typopro-web/web",
+                    { expand: true, flatten: false, cwd: "node_modules/typopro-web/web",
                       src: "**", dest: "specimen/" },
                 ]
             },
             "typopro-index": {
                 files: [
-                    { dest: "specimen/index.html",    src: [ "bower_components/typopro-web/index.html" ] },
-                    { dest: "specimen/specimen.html", src: [ "bower_components/typopro-web/specimen.html" ] }
+                    { dest: "specimen/index.html",    src: [ "node_modules/typopro-web/index.html" ] },
+                    { dest: "specimen/specimen.html", src: [ "node_modules/typopro-web/specimen.html" ] }
                 ]
             },
             "typopro-index-manifest": {
-                src: [ "bower_components/typopro-web/manifest.js" ],
+                src: [ "node_modules/typopro-web/manifest.js" ],
                 dest: "specimen/manifest.js",
                 options: {
                     process: function (content, srcpath) {
@@ -53,10 +43,9 @@ module.exports = function (grunt) {
         },
         clean: {
             clean:     [ "lib" ],
-            distclean: [ "node_modules", "bower_components" ]
+            distclean: [ "node_modules" ]
         }
-    });
-
-    grunt.registerTask("default", [ "bower-install-simple", "copy" ]);
-};
+    })
+    grunt.registerTask("default", [ "copy" ])
+}
 
